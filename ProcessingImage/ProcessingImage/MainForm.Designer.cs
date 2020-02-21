9a515f2bee7be9f27ed890660707c2759638513d
kageyusa <kageyusa@fsi.co.jp>
2020-02-21 13:49:21 +0900

    画像表示

--- ProcessingImage/ProcessingImage/MainForm.Designer.cs
+++ ProcessingImage/ProcessingImage/MainForm.Designer.cs
@@ -32,14 +32,17 @@
             this.pictureBox = new System.Windows.Forms.PictureBox();
             this.SelectImageButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.GrayScale = new System.Windows.Forms.ToolStripMenuItem();
-            this.二値化ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GrayScaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GrayBrightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GrayLuminanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BinaryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.モザイクToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.エッジ抽出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.回転ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.z軸周りToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.明るさ調整ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.コントラストToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.SaveImage = new System.Windows.Forms.Button();
+            this.SaveImageButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -64,14 +67,15 @@
             this.SelectImageButton.TabIndex = 4;
             this.SelectImageButton.Text = "画像選択";
             this.SelectImageButton.UseVisualStyleBackColor = true;
+            this.SelectImageButton.Click += new System.EventHandler(this.SelectImageButton_Click);
             // 
             // menuStrip1
             // 
             this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(80, 8, 0, 2);
             this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.GrayScale,
-            this.二値化ToolStripMenuItem,
+            this.GrayScaleToolStripMenuItem,
+            this.BinaryToolStripMenuItem,
             this.モザイクToolStripMenuItem,
             this.エッジ抽出ToolStripMenuItem,
             this.回転ToolStripMenuItem,
@@ -84,17 +88,32 @@
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // GrayScale
+            // GrayScaleToolStripMenuItem
             // 
-            this.GrayScale.Name = "GrayScale";
-            this.GrayScale.Size = new System.Drawing.Size(93, 20);
-            this.GrayScale.Text = "グレースケール化";
+            this.GrayScaleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GrayBrightToolStripMenuItem,
+            this.GrayLuminanceToolStripMenuItem});
+            this.GrayScaleToolStripMenuItem.Name = "GrayScaleToolStripMenuItem";
+            this.GrayScaleToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.GrayScaleToolStripMenuItem.Text = "グレースケール化";
             // 
-            // 二値化ToolStripMenuItem
+            // GrayBrightToolStripMenuItem
             // 
-            this.二値化ToolStripMenuItem.Name = "二値化ToolStripMenuItem";
-            this.二値化ToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
-            this.二値化ToolStripMenuItem.Text = "二値化";
+            this.GrayBrightToolStripMenuItem.Name = "GrayBrightToolStripMenuItem";
+            this.GrayBrightToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.GrayBrightToolStripMenuItem.Text = "明度変換";
+            // 
+            // GrayLuminanceToolStripMenuItem
+            // 
+            this.GrayLuminanceToolStripMenuItem.Name = "GrayLuminanceToolStripMenuItem";
+            this.GrayLuminanceToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.GrayLuminanceToolStripMenuItem.Text = "輝度変換";
+            // 
+            // BinaryToolStripMenuItem
+            // 
+            this.BinaryToolStripMenuItem.Name = "BinaryToolStripMenuItem";
+            this.BinaryToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.BinaryToolStripMenuItem.Text = "二値化";
             // 
             // モザイクToolStripMenuItem
             // 
@@ -110,9 +129,17 @@
             // 
             // 回転ToolStripMenuItem
             // 
+            this.回転ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.z軸周りToolStripMenuItem});
             this.回転ToolStripMenuItem.Name = "回転ToolStripMenuItem";
             this.回転ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.回転ToolStripMenuItem.Text = "回転";
+            // 
+            // z軸周りToolStripMenuItem
+            // 
+            this.z軸周りToolStripMenuItem.Name = "z軸周りToolStripMenuItem";
+            this.z軸周りToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.z軸周りToolStripMenuItem.Text = "Z軸周り";
             // 
             // 明るさ調整ToolStripMenuItem
             // 
@@ -126,21 +153,23 @@
             this.コントラストToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
             this.コントラストToolStripMenuItem.Text = "コントラスト";
             // 
-            // SaveImage
+            // SaveImageButton
             // 
-            this.SaveImage.Location = new System.Drawing.Point(885, 1);
-            this.SaveImage.Name = "SaveImage";
-            this.SaveImage.Size = new System.Drawing.Size(75, 23);
-            this.SaveImage.TabIndex = 6;
-            this.SaveImage.Text = "画像保存";
-            this.SaveImage.UseVisualStyleBackColor = true;
+            this.SaveImageButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveImageButton.Location = new System.Drawing.Point(885, 1);
+            this.SaveImageButton.Name = "SaveImageButton";
+            this.SaveImageButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveImageButton.TabIndex = 6;
+            this.SaveImageButton.Text = "画像保存";
+            this.SaveImageButton.UseVisualStyleBackColor = true;
+            this.SaveImageButton.Click += new System.EventHandler(this.SaveImageButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(964, 571);
-            this.Controls.Add(this.SaveImage);
+            this.Controls.Add(this.SaveImageButton);
             this.Controls.Add(this.SelectImageButton);
             this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.menuStrip1);
@@ -160,14 +189,17 @@
         private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.Button SelectImageButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem GrayScale;
-        private System.Windows.Forms.ToolStripMenuItem 二値化ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GrayScaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem BinaryToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem モザイクToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem エッジ抽出ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 回転ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 明るさ調整ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem コントラストToolStripMenuItem;
-        private System.Windows.Forms.Button SaveImage;
+        private System.Windows.Forms.Button SaveImageButton;
+        private System.Windows.Forms.ToolStripMenuItem GrayBrightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GrayLuminanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem z軸周りToolStripMenuItem;
     }
 }
 
--- ProcessingImage/ProcessingImage/MainForm.cs
+++ ProcessingImage/ProcessingImage/MainForm.cs
@@ -12,10 +12,49 @@ namespace ProcessingImage
 {
     public partial class MainForm : Form
     {
+        Bitmap img;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
+        //画像選択
+        private void SelectImageButton_Click(object sender, EventArgs e)
+        {
+            String path = SelectFile();
+            if (path == null) return;
+
+            img = new Bitmap(path);
+
+            pictureBox.Image = img;
+        }
+        //画像保存
+        private void SaveImageButton_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    Console.WriteLine(sfd.FileName);
+
+                    pictureBox.Image.Save(sfd.FileName);
+                }
+            }
+        }
+
+        //ファイルのpath渡し用のメソッド
+        private String SelectFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
